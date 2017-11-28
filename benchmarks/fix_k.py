f86d901722dbc09fb260f826f551c2fe97a195f4
Jack River <jack.zz.river@gmail.com>
2017-11-28 10:38:40 +0800

    to one millions

--- benchmarks/fix_k.py
+++ benchmarks/fix_k.py
@@ -12,4 +12,3 @@ for x in range(1,101):
     nx.write_edgelist(nx.random_graphs.barabasi_albert_graph(i,k), "BA_{0}_{1}.csv".format(i,k), delimiter=",", data = False)
     nx.write_edgelist(nx.random_graphs.erdos_renyi_graph(i,2.0*k/i), "ER_{0}_{1}.csv".format(i,k), delimiter=",", data = False) 
     nx.write_edgelist(nx.random_graphs.watts_strogatz_graph(i,2*k,0.1), "WS_{0}_{1}.csv".format(i,k), delimiter=",", data = False)
-    nx.write_edgelist(nx.random_graphs.watts_strogatz_graph(i,2*k,0.1), "WS_{0}_{1}.csv".format(i,k), delimiter=",", data = False)
--- benchmarks/run_fix_k.sh
+++ benchmarks/run_fix_k.sh
@@ -2,13 +2,19 @@
 
 echo "model,method,attack,n,k,R,time"
 
-for i in `seq 10000 10000 190000`
+for i in `seq 10000 10000 1000000`
 do
+    echo $i >&2
+    echo BA HDA >&2
     echo $i `../fast -h BA_${i}_100.csv -t` | xargs | awk '{print "BA,Fast-R,HDA," $1 "," 100 "," $2 "," $3}'
+    echo ER HDA >&2
     echo $i `../fast -h ER_${i}_100.csv -t` | xargs | awk '{print "ER,Fast-R,HDA," $1 "," 100 "," $2 "," $3}'
+    echo WS HDA >&2
     echo $i `../fast -h WS_${i}_100.csv -t` | xargs | awk '{print "WS,Fast-R,HDA," $1 "," 100 "," $2 "," $3}'
-
+    echo BA RA >&2
     echo $i `../fast -r BA_${i}_100.csv -t` | xargs | awk '{print "BA,Fast-R,RA," $1 "," 100 "," $2 "," $3}'
+    echo ER RA >&2
     echo $i `../fast -r ER_${i}_100.csv -t` | xargs | awk '{print "ER,Fast-R,RA," $1 "," 100 "," $2 "," $3}'
+    echo WS RA >&2
     echo $i `../fast -r WS_${i}_100.csv -t` | xargs | awk '{print "WS,Fast-R,RA," $1 "," 100 "," $2 "," $3}'
 done
