bac7e452259ad2c9a24a25a8f108a0b14c8d5670
Jack River <jack.zz.river@gmail.com>
2017-11-28 10:36:52 +0800

    remove repeated lines

--- benchmarks/fix_n.py
+++ benchmarks/fix_n.py
@@ -11,4 +11,3 @@ for k in range(1,101):
     nx.write_edgelist(nx.random_graphs.barabasi_albert_graph(i,k), "BA_{0}_{1}.csv".format(i,k), delimiter=",", data = False)
     nx.write_edgelist(nx.random_graphs.erdos_renyi_graph(i,2.0*k/i), "ER_{0}_{1}.csv".format(i,k), delimiter=",", data = False) 
     nx.write_edgelist(nx.random_graphs.watts_strogatz_graph(i,2*k,0.1), "WS_{0}_{1}.csv".format(i,k), delimiter=",", data = False)
-    nx.write_edgelist(nx.random_graphs.watts_strogatz_graph(i,2*k,0.1), "WS_{0}_{1}.csv".format(i,k), delimiter=",", data = False)
